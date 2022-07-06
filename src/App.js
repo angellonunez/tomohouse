085120f15f519787751e06da7e939c49b40989ec
Angello Nunez <angellonzgz@gmail.com>
2022-07-06 00:01:09 -0400

    Add NavMenu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import FriendsMenu from './components/FriendsMenu';
 import HousesSection from './components/HousesSection';
+import NavMenu from './components/NavMenu';
 
 
 function App() {
   return (
     <div className="App">
+      <NavMenu />
       <FriendsMenu />
       <HousesSection />
     </div>
---
+++ src/components/NavMenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NavMenu.css';
+
+function NavMenu() {
+    return (
+        <div className='navMenu'>
+            <i class="fa-solid fa-bars menu"></i>
+            <i class="fa-solid fa-horse-head"></i>
+            <img src='/img/angellonunez.jpg'/>
+        </div>
+    )
+}
+
+export default NavMenu;
