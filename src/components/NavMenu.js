28b93a382c91854d7d661ab24ee1640354b09a72
Angello Nunez <angellonzgz@gmail.com>
2022-07-09 17:42:47 -0400

    Test new design

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -4,9 +4,22 @@ import './NavMenu.css';
 function NavMenu() {
     return (
         <div className='navMenu'>
-            <i class="fa-solid fa-bars menu"></i>
-            <i class="fa-solid fa-horse-head"></i>
-            <img src='/img/angellonunez.jpg'/>
+            <div className='nav-box first-icon'>
+                <i class="fa-brands fa-hornbill"></i>
+                <p>Houses</p>
+            </div>
+            <div className='nav-box'>
+                <i class="fa-brands fa-pushed"></i> 
+                <p>Parties</p>
+            </div>
+            <div className='nav-box'>
+            <i class="fa-regular fa-message"></i>
+                <p>Chat</p>
+            </div>
+            <div className='nav-box last-icon'>
+                <img src='/img/angellonunez.jpg'/>
+                <p>Profile</p>
+            </div>
         </div>
     )
 }
